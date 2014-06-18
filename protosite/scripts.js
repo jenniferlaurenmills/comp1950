831700dbef8ef9a2ec1ecf2dbe304e8092442d97
ali-sharifi <sharifi.ali@hotmail.com>
2014-06-18 12:27:47 -0700

    foldering and indents
    adding some folders and some indents

---
+++ protosite/scripts.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('.listMenu > li').bind('mouseover', openSubMenu);
+    $('.listMenu > li').bind('mouseout', closeSubMenu);
+
+});
+
+function openSubMenu() {
+    $(this).find('ul').css('visibility', 'visible');
+};
+
+function closeSubMenu() {
+    $(this).find('ul').css('visibility', 'hidden');
+};

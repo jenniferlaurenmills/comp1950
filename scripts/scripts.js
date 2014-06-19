6aaf4f31bcc74ea42731d8681d16f5a25881c6d6
ali-sharifi <sharifi.ali@hotmail.com>
2014-06-19 02:08:59 -0700

    Team home site redesign
    I hope Mr. Teng doesn't get mad!:S

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,23 @@
 ﻿
 //changing the navigation on scroll
 $(document).ready(function () {
+    $('#secondnav').css("display", "none");
+
     $(window).scroll(function () {
         var sT = $(this).scrollTop();
         if (sT >= 220) {
-            $('#mainnav').addClass('hidenav');
-            $('#secondnav').css("display", "block");
+            //$('#mainnav').addClass('hidenav');
+            //$('#secondnav').css("display", "block");
+            // $('#secondnav').css("display", "block").fadeIn("slow");
+            $('#secondnav').show("slow").fadein("slow");
+            
 
         } else {
-            $('#mainnav').removeClass('hidenav');
-            //$('#secondnav').removeClass('shownav');
-            $('#secondnav').css("display", "none");
+            //$('#mainnav').removeClass('hidenav');
+            //$('#secondnav').fadein(300);
+            //$('#secondnav').css("display", "none").fadeOut("slow");
+            $('#secondnav').hide("slow").fadeOut("slow");
+
         }
     })
 })

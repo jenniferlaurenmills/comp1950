41bff221f6a74f94b8eb4d5210072eb01fde1aa4
ali-sharifi <sharifi.ali@hotmail.com>
2014-06-19 00:22:13 -0700

    adding some feature to home navigation
    secondary nav for home page with jquery!
    I love foldering!!!

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+﻿
+//changing the navigation on scroll
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var sT = $(this).scrollTop();
+        if (sT >= 220) {
+            $('#mainnav').addClass('hidenav');
+            $('#secondnav').css("display", "block");
+
+        } else {
+            $('#mainnav').removeClass('hidenav');
+            //$('#secondnav').removeClass('shownav');
+            $('#secondnav').css("display", "none");
+        }
+    })
+})

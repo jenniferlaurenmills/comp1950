fa9d77f5ddd70c55b6d0b91f0654e39327ce523c
ali-sharifi <sharifi.ali@hotmail.com>
2014-06-20 16:57:58 -0700

    almost there with homepage of proto

--- protosite/scripts/scripts.js
+++ protosite/scripts/scripts.js
@@ -1,8 +1,18 @@
 ﻿
+//////////////Menu//////////////
 $(document).ready(function () {
     $('.listMenu > li').bind('mouseover', openSubMenu);
     $('.listMenu > li').bind('mouseout', closeSubMenu);
 
+    /////////for change color of border on transparent buttons///////////
+    $("#lnk1").mouseover(function () { $("#btn1").css("border-color", "#00d0ff");});
+    $("#lnk1").mouseout(function () { $("#btn1").css("border-color", "#fff"); });
+    $("#lnk2").mouseover(function () { $("#btn2").css("border-color", "#00d0ff"); });
+    $("#lnk2").mouseout(function () { $("#btn2").css("border-color", "#fff"); });
+    $("#lnk3").mouseover(function () { $("#btn3").css("border-color", "#00d0ff"); });
+    $("#lnk3").mouseout(function () { $("#btn3").css("border-color", "#fff"); });
+    $("#lnk4").mouseover(function () { $("#btn4").css("border-color", "#00d0ff"); });
+    $("#lnk4").mouseout(function () { $("#btn4").css("border-color", "#fff"); });
 });
 
 function openSubMenu() {
@@ -13,7 +23,7 @@ function closeSubMenu() {
     $(this).find('ul').css('visibility', 'hidden');
 };
 
-
+//////////////for Footer Slide///////////////////
 $(window).scroll(function () {
     if ($(this).scrollTop() < 50) {
 
@@ -26,8 +36,8 @@ $(window).scroll(function () {
     }
 });
 
-
-
+//////////////for Carousel ///////////////////
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
+

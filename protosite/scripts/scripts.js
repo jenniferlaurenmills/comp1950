bc3e4813e31d47c93b65ece7c2b9e21298e68019
ali-sharifi <sharifi.ali@hotmail.com>
2014-06-21 00:58:20 -0700

    Revamp of Index

--- protosite/scripts/scripts.js
+++ protosite/scripts/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.listMenu > li').bind('mouseout', closeSubMenu);
 
     /////////for change color of border on transparent buttons///////////
-    $("#lnk1").mouseover(function () { $("#btn1").css("border-color", "#00d0ff");});
+    $("#lnk1").mouseover(function () { $("#btn1").css("border-color", "#00d0ff"); });
     $("#lnk1").mouseout(function () { $("#btn1").css("border-color", "#fff"); });
     $("#lnk2").mouseover(function () { $("#btn2").css("border-color", "#00d0ff"); });
     $("#lnk2").mouseout(function () { $("#btn2").css("border-color", "#fff"); });
@@ -13,6 +13,7 @@ $(document).ready(function () {
     $("#lnk3").mouseout(function () { $("#btn3").css("border-color", "#fff"); });
     $("#lnk4").mouseover(function () { $("#btn4").css("border-color", "#00d0ff"); });
     $("#lnk4").mouseout(function () { $("#btn4").css("border-color", "#fff"); });
+
 });
 
 function openSubMenu() {
@@ -40,4 +41,7 @@ $(window).scroll(function () {
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
+
+
+
 
